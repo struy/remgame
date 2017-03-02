66a9e7b8fb0596dc9e068f2f061bb7bf0632b6f0
Oleksandr Dubovyk <struykm@yandex.ru>
2017-03-02 15:43:39 +0200

    add FAQ.gif add images for button

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import os
-from tkinter import  *
+from tkinter import *
+from random import shuffle
+
+SIDE = 8  # size of square
+QSIDE = SIDE**2//2  # count of uniq images
 
 
-print(os.listdir('gif'))
-flles = [os.path.join('gif', f) for f in os.listdir('gif')]
+def cmd():
+    print('Yes')
+    print('....')
 
 main_window = Tk()
 main_window.title('Remgame')
-for i in range(10):
-    for j in range(10):
-        btn = Button(main_window, text='?', font=('Courier New', 14, 'normal'), relief=FLAT)
+
+faq = PhotoImage(file='FAQ.gif')
+
+files = [os.path.join('gif', f) for f in os.listdir('gif')]
+shuffle(files)
+files = files[0:QSIDE]*2
+shuffle(files)
+
+images = [PhotoImage(file=f) for f in files]
+
+for i in range(SIDE):
+    for j in range(SIDE):
+        btn = Button(main_window, image=images[i * SIDE + j], relief=FLAT, command=cmd)
         btn.grid(row=i, column=j)
 
 main_window.mainloop()
