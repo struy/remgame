174f6786e603c501566c2c21a1c9ebb319965bc9
Oleksandr Dubovyk <struykm@yandex.ru>
2017-03-02 17:48:06 +0200

    add function hide _all for images after 2000 ms

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ QSIDE = SIDE**2//2  # count of uniq images
 
 
 def cmd():
-    print('Yes')
-    print('....')
+    pass
+
+def hide_all(buttons):
+    for btn in buttons:
+        btn.config(images=faq)
+
 
 main_window = Tk()
 main_window.title('Remgame')
@@ -21,11 +25,18 @@ files = files[0:QSIDE]*2
 shuffle(files)
 
 images = [PhotoImage(file=f) for f in files]
+buttons =[]
+
 
 for i in range(SIDE):
     for j in range(SIDE):
         btn = Button(main_window, image=images[i * SIDE + j], relief=FLAT, command=cmd)
         btn.grid(row=i, column=j)
+        buttons.append(btn)
 
+
+
+
+main_window.after(2000,hide_all,buttons)
 main_window.mainloop()
 
